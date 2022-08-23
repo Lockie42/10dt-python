910c3f2e2093785e15c7b787626b917ffb63d129
Lockie42 <103463157+Lockie42@users.noreply.github.com>
2022-08-24 10:36:48 +1200

    Add files via upload

---
+++ lesson 1/python.practice.py
@@ -0,0 +1,16 @@
+# l Sainsbury
+# practise task - variables, print, data type,
+
+#Variable declaration
+Name = "Lockie Sainsbury"
+height = 177
+Phone = "021 085 78192"
+address = "207 Houghton Bay Road, Wellington, NZ"
+allergy = "No"
+
+#print to screen
+print('My name:',Name)
+print("Height in centimetres:",height)
+print("My schools phone number:", Phone)
+print("My schools address:", address)
+print("Do I have any allergys?:", allergy)
