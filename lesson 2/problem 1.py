472ed4d7d15acce8d0245fffc2324cc8a4c42fcc
Lockie42 <103463157+Lockie42@users.noreply.github.com>
2022-09-07 10:53:06 +1200

    Add files via upload

---
+++ lesson 2/problem 1.py
@@ -0,0 +1,11 @@
+#Lockie Sainsbury
+#Cacultion of variables, farming of labels, data in context
+
+#problem 01
+#This problem is designed to track and total goals by scots #rd XI for each game played
+
+match1_ST_Pats = 7
+match2_Well_college = 5 
+match3_Well_High = 10
+Team_total = match1_ST_Pats + match2_Well_college + match3_Well_High
+print ("total Goals scored ", Team_total)
