0a6e4718d8ab7f1b057f170b6c92869bbd3e720a
Lockie42 <103463157+Lockie42@users.noreply.github.com>
2022-09-08 12:14:18 +1200

    Add files via upload

---
+++ lesson 2/problem 2.py
@@ -0,0 +1,13 @@
+#Lockie Sainsbury
+#pepresting number data type as string data types in the output
+
+#problem 02
+#This program is designed to track thew value remaining on a gift card as it is used
+
+card_Balance = 50.00
+purchase_01 = 35.00
+print("your current Balance: $" + str (card_Balance))
+
+Card_Balance = card_Balance - purchase_01
+print ("purchase_01 $" + str (purchase_01))
+print("your new balance is: $" + str (Card_Balance))
